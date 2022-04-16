149561c23ac4cfd748a2cd957362245717fad356
barbadeadesh <barbadeadesh2020@email.com>
2022-04-16 20:09:24 +0530

    ADDED: Functionality for  calculate fare for multiple ride

---
+++ Invoice.java
@@ -0,0 +1,32 @@
+public class InvoiceGeneratorr {
+
+	/*
+	 * Given : MINIMUM_COST_PER_KM = 10.0 COST_PER_TIME = 1 MINIMUM_FARE = 5
+	 */
+	private static final double MINIMUM_COST_PER_KM = 10.0;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
+
+	/*
+	 * Purpose : Given distance and time, Return total fare for the journey <p>
+	 * Condition : If minimum total fare is less than the MINIMUM_FARE, return
+	 * MINIMUM_FARE
+	 */
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+
+		return Math.max(totalFare, MINIMUM_FARE);
+	}
+
+	/*
+	 * Purpose : Given distance and time for multiple rides, Return aggregate total
+	 * fare for all the journey
+	 */
+	public double calculateTotalFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
+}
